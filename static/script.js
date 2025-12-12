acd9873aa92b4e9c0a00750175662571e09e7c9e
defaultyt1234 <defaultyt1234@gmail.com>
2025-12-13 00:44:50 +0530

    Update script.js

--- static/script.js
+++ static/script.js
@@ -1,1 +1,9 @@
-console.log("Smart Attendance Loaded");
+function takeAttendance() {
+    fetch('/take_attendance', { method: 'POST' })
+        .then(response => response.json())
+        .then(data => {
+            alert('Attendance Updated!');
+            location.reload(); // reload page to show new records
+        })
+        .catch(error => console.error('Error:', error));
+}
